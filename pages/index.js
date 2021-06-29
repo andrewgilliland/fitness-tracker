c0a233193fdd0af3fbbfca879dda97687b302811
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-06-29 17:51:24 -0500

    adds db into ss props

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,18 @@ import Program from "../components/Program";
 import { program } from "../data/programs";
 import { connectToDatabase } from "../util/mongodb";
 
-export default function Home({ isConnected }) {
+export default function Home({ isConnected, programs }) {
+  console.log(programs);
   return (
     <div className="min-h-screen bg-black text-gray-50 z-10">
       <Head>
-        <title>Fitness Tracker</title>
+        <title>Fitness Tracker | {programs[0].name}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className=" flex flex-col px-2 py-10">
         <div className="mx-auto text-center py-5">
           <h1 className="text-xl font-semibold py-5">Fitness Tracker</h1>
-          {isConnected ? (
-            <h2 className="subtitle">You are connected to MongoDB</h2>
-          ) : (
-            <h2 className="subtitle">
-              You are NOT connected to MongoDB. Check the <code>README.md</code>{" "}
-              for instructions.
-            </h2>
-          )}
         </div>
         <Program program={program} />
       </main>
@@ -29,12 +22,13 @@ export default function Home({ isConnected }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase();
-
+export async function getServerSideProps() {
+  const { client, db } = await connectToDatabase();
   const isConnected = await client.isConnected();
 
+  const programs = await db.collection("programs").find({}).limit(20).toArray();
+
   return {
-    props: { isConnected },
+    props: { isConnected, programs: JSON.parse(JSON.stringify(programs)) },
   };
 }
